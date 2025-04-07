cec56a4e34ffd7050fdc5e4d8ade4d10a8cc24f1
Bonifacio Conde <b.conde@foodics.com>
2025-04-07 19:03:07 +0800

    Update push new place + action button

--- lib/screens/new_place.dart
+++ lib/screens/new_place.dart
@@ -5,6 +5,25 @@ class NewPlace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('New Place')));
+    return Scaffold(
+      appBar: AppBar(title: Text('New Place')),
+      body: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                label: Text(
+                  'Place name',
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyMedium!.copyWith(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.onSecondary,
+    );
   }
 }
--- lib/screens/place_list.dart
+++ lib/screens/place_list.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:my_favorite_places/screens/new_place.dart';
 
 class PlaceList extends StatelessWidget {
   const PlaceList({super.key});
 
+  void _navigateNewPlace(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (ctx) => NewPlace()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your Places')),
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _navigateNewPlace(context);
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
       backgroundColor: Theme.of(context).colorScheme.onSecondary,
     );
   }
