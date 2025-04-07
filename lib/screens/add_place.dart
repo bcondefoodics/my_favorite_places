57cb40fae576d62d2ffad329e91a2fdcb2f964ca
Bonifacio Conde <b.conde@foodics.com>
2025-04-07 19:25:43 +0800

    Update textfield + button

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
-class AddPlaceScreen extends StatelessWidget {
+class AddPlaceScreen extends StatefulWidget {
   const AddPlaceScreen({super.key});
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Place')),
-      body: Form(
+      appBar: AppBar(title: Text('Add new place')),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
         child: Column(
           children: [
-            TextFormField(
-              decoration: InputDecoration(
-                label: Text(
-                  'Place name',
-                  style: Theme.of(
-                    context,
-                  ).textTheme.bodyMedium!.copyWith(color: Colors.white),
-                ),
-              ),
+            TextField(
+              decoration: InputDecoration(label: Text('Title')),
+              controller: _titleController,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton.icon(
+              icon: Icon(Icons.send),
+              onPressed: () {},
+              label: Text('Add Place'),
             ),
           ],
         ),
